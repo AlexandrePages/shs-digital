60f2392c898fe37775612aba35049fbf4fc9f7f1
Alexandre Pages <alexandre.pages@epfl.ch>
2018-05-12 20:44:14 +0200

    ayoooooo

--- PhraseFinder.py
+++ PhraseFinder.py
@@ -4,158 +4,21 @@ Spyder Editor
 
 """
 import math
-
-def clean_up_sen(a):
-    a = a.replace("- ","")
-    a = a.replace(":", " ")
-    a = a.replace("\n", "")
-    a = a.split(" ")
-    return a
-
-
-#def find_date(lines,DATE):  
-#    for i in range(0,len(lines)):
-#        for j in range(0,len(Jour)):
-#            if Jour[j] in lines[i]:
-#                linedate = re.split('\W+',lines[i])
-#                for i in range(0,len(linedate)):
-#                    for m in range(0,len(Mois)):
-#                        if Jour[j] in linedate[i] and Mois[m] in linedate[i+2]:
-#                            DATE = linedate[i] +' '+ linedate[i+1] + ' ' + Mois[m] + ' ' + linedate[i+3]
-#     
-#    return DATE
-
-def find_date(fulltext,sen,index_text,date_tab):
-    DATE = 0
-    found = 0
-    #AnalyseDate = re.split('\W+',sen)
-    AnalyseDate = sen
-    for i in range(len(AnalyseDate)):
-        if len(AnalyseDate[i]) == 4 and AnalyseDate[i].isdigit():
-            DATE = AnalyseDate[i]
-            found = 1
-    if len(date_tab) != 0 and found == 0:
-        DATE = date_tab[len(date_tab)-1]
-        found = 0
-        return DATE
-    if DATE == 0 and found == 0:
-        DATE = 'None'            
-    return DATE
-
-
-def find_pays(fulltext,sen,index_text,pays_tab):
-    PAYS = 0
-    found = 0
-    #AnalyseDate = re.split('\W+',sen)
-    AnalysePays = sen
-    for i in range(len(AnalysePays)):
-        if AnalysePays[i] in Pays:
-            PAYS = AnalysePays[i]
-            found = 1
-    if len(pays_tab) != 0 and found == 0:
-        PAYS = pays_tab[len(pays_tab)-1]
-        found = 0
-        return PAYS
-    if PAYS == 0 and found == 0:
-        PAYS = 'None'            
-    return PAYS
-
-
-def analyse_sen(sen,DateTab,PaysFinal_):
-    TailleSen_= 0
-    PaysFinal_ = []
-    for j in range(0,len(sen)):
-            AnalyseSen = sen[j]
-            AnalyseSen = clean_up_sen(AnalyseSen)
-            AnalyseSen = re.split('\W+',sen[j])
-            PaysFinal_.append(find_pays(sen,AnalyseSen,j,PaysFinal_))
-            DateTab.append(find_date(sen,AnalyseSen,j,DateTab)) #FIND DATE
-            TailleSen_ += len(AnalyseSen) 
-            AnalyseDate = re.split('\W+',sen[j])
-#                if len(AnalyseDate[i]) == 4 and AnalyseDate[i].isdigit():
-            #for i in range(0,len(AnalyseDate)):
- #                       DATEFinal_ = AnalyseDate[i]
-            for k in range(0,len(AnalyseSen)):
-                SenFull.append(AnalyseSen[k])
-    return TailleSen_, DateTab,PaysFinal_
-
-
-def Occurence(sen,DateTab,PaysTab):
-     #sort by occurence
-    words = []
-    wordsocc = []
-    sen_tot_occ = []
-    sen_sort = []
-    Occ = 0
-    for k in range(0,len(sen)):
-        sen_sort += clean_up_sen(sen[k])
-    #words.append(sen_sort[0])
-    for i in range(0,len(sen_sort)):
-        if sen_sort[i] not in words:
-          words.append(sen_sort[i])
-                
-    for i in range(0,len(words)):
-        Occ = 0
-        for j in range(0,len(sen_sort)):
-            if words[i] == sen_sort[j]:
-                Occ += 1
-        wordsocc.append(Occ)
+import re
+import SentenceAnalyzer as SA
+Pays = []
+Capi = []
+with open("pays.txt", 'r') as pays_file:
+    Pays = pays_file.read()
+    Pays = Pays.split('\n')
     
+with open("Capitales.txt", 'r') as Capi_file:
+    Capi = Capi_file.read()
+    Capi = Capi.split('\n')
     
-    for i in range(0,len(sen)):
-        sen_tot_occ.append(0)
-        for j in range(0,len(words)):
-            if words[j] in sen[i]:
-                sen_tot_occ[i] += wordsocc[i]
-            
-    for i in range(0,len(sen)):
-        sen_tot_occ[i] = sen_tot_occ[i]/len(sen[i])
-            
-    T = []
-    T = [x for _,x in sorted(zip(sen_tot_occ,sen))]
-    
-    Date = []
-    Date = [x for _,x in sorted(zip(sen_tot_occ,DateTab))]
-    Pays = []
-    Pays = [x for _,x in sorted(zip(sen_tot_occ,PaysTab))]
-    Tot_occ = sorted(sen_tot_occ)
-    return T, Tot_occ,Date,Pays
-    
-
-
-
-def NameOccExp(Sen,Occ,Name,DateTab,PaysTab):
-    for i in range(0,len(Sen)):
-        AnalyseName = re.split('\W+',Sen[i])
-        for j in range(0,len(AnalyseName)):
-            if Name == AnalyseName[j]:
-                Occ[i] -= 10
-                index = i
-                for k in range(0,len(Sen)):
-                    if k != index:
-                        Occ[k] -= math.exp(-abs(index-k))
-    T = []
-    T = [x for _,x in sorted(zip(Occ,Sen))]
-    Date = []
-    Date = [x for _,x in sorted(zip(Occ,DateTab))]
-    Pays = []
-    Pays = [x for _,x in sorted(zip(Occ,PaysTab))]
-    Tot_occ = sorted(Occ)
-    return T, Tot_occ,Date,Pays
-            
-#------------------------------------------------------------------------------
-    #------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
-def clean_up(a):
-    a = a.replace("- ","")
-    a = a.replace("\n","")
-    return a
-#------------------------------------------------------------------------------
-    #-----------------------------------------------------------------------------
-#------------------------------------------------------------------------------
 Jour = ['LUNDI','MARDI', 'MERCREDI', 'JEUDI', 'VENDREDI', 'SAMEDI','DIMANCHE']
-Mois = ['JANVIER', 'FEVRIER', 'MARS', 'AVRIL', 'MAI', 'JUIN','JUILLET', 'AOÛT','AOUT', 'SEPTEMBRE', 'OCTOBRE', 'NOVEMBRE', 'DECEMBRE']
-Pays = [];
+Mois = ['janvier','février','fevrier','mars','avril','mai','juin','juillet','août','aout','septembre','octobre','novembre','décembre','decembre']
+
 
 length_sen = []
 sen = []
@@ -171,10 +34,181 @@ DATEFinal = ''
 Sorted_sen = []
 Sorted_occ = []
 
-import SentenceAnalyzer as SA
+
+def clean_up_sen(a):
+    a = a.replace("- ","")
+    a = a.replace(":", " ")
+    a = a.replace("\n", "")
+    a = a.split(" ")
+    return a
+
+#------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 
-def Define_AbsOcc(NS,AO,DT,PF):
+
+def find_date(fulltext,sen,index_text,date_tab):
+    DATE = str(0)
+    DateTab = ['None','None','None']
+    found = 0
+    AnalyseDate = sen
+    for i in range(len(AnalyseDate)):
+        if len(AnalyseDate[i]) == 4 and AnalyseDate[i].isdigit():
+            DateTab[2] = str(AnalyseDate[i])
+            found = 1
+        if found == 1:
+            for j in range(len(Mois)):
+                if AnalyseDate[i].lower() == Mois[j]:
+                    if j <2 :
+                        DateTab[1] = '0' + str(j+1)
+                    if j >= 2 and j <= 8:
+                        DateTab[1] = '0' + str(j)
+                    if j == 9:
+                        DateTab[1] = '0' + str(j-1)
+                    if j >= 10:
+                        DateTab[1] = str(j-1)
+                    if j == 14:
+                        DateTab[1] = str(j-2)
+                    found = 1
+            if (len(AnalyseDate[i]) <= 2 and len(AnalyseDate[i]) > 0) and AnalyseDate[i].isdigit() and i < len(AnalyseDate)-1:
+                if AnalyseDate[i+1].lower() in Mois:
+                    DateTab[0] = str(AnalyseDate[i])
+                    found = 1  
+    if len(date_tab) != 0 and found == 0:
+        DATE = date_tab[len(date_tab)-1]
+        found = 0
+        return DATE     
+    
+    
+    
+    DATE = DateTab[0] + '/' +   DateTab[1]  + '/' +    DateTab[2]
+    return DATE
+
+#------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+def find_pays(fulltext,sen,index_text,pays_tab):
+    PAYS = 'None'
+    CAP = 'None'
+    found = 0
+    AnalysePays = sen
+    for i in range(len(AnalysePays)):
+        if AnalysePays[i] in Pays:
+            PAYS = AnalysePays[i]
+            found = 1
+    for i in range(len(AnalysePays)):
+        if AnalysePays[i] in Capi:
+            CAP = AnalysePays[i]
+            found = 1        
+    if len(pays_tab) != 0 and found == 0:
+        PAYS = pays_tab[len(pays_tab)-1]
+        found = 0
+        return PAYS
+    if CAP != 'None':
+        PAYS = CAP
+              
+    return PAYS
+
+#------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+
+def analyse_sen(sen,DateTab,PaysFinal_):
+    TailleSen_= 0
+    PaysFinal_ = []
+    for j in range(0,len(sen)):
+            AnalyseSen = sen[j]
+            AnalyseSen = clean_up_sen(AnalyseSen)
+            AnalyseSen = re.split('\W+',sen[j])
+            PaysFinal_.append(find_pays(sen,AnalyseSen,j,PaysFinal_))
+            DateTab.append(find_date(sen,AnalyseSen,j,DateTab)) #FIND DATE
+            TailleSen_ += len(AnalyseSen) 
+
+            for k in range(0,len(AnalyseSen)):
+                SenFull.append(AnalyseSen[k])
+    return  DateTab,PaysFinal_
+
+
+#------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+def Occurence(sen):
+     #sort by occurence
+    words = []
+    wordsocc = []
+    sen_tot_occ = []
+    sen_sort = []
+    Occ = 0
+    for k in range(0,len(sen)):
+        sen_sort += clean_up_sen(sen[k])
+    for i in range(0,len(sen_sort)):
+        if sen_sort[i] not in words:
+          words.append(sen_sort[i])
+                
+    for i in range(0,len(words)):
+        Occ = 0
+        for j in range(0,len(sen_sort)):
+            if words[i] == sen_sort[j]:
+                Occ -= 1
+        wordsocc.append(Occ)
+    
+    
+    for i in range(0,len(sen)):
+        sen_tot_occ.append(0)
+        for j in range(0,len(words)):
+            if words[j] in sen[i]:
+                sen_tot_occ[i] -= wordsocc[i]
+            
+    for i in range(0,len(sen)):
+        sen_tot_occ[i] = sen_tot_occ[i]/len(sen[i])
+ 
+    return sen,sen_tot_occ
+    
+
+#------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+
+def NameOccExp(Sen,Occ,Name):
+    for i in range(0,len(Sen)):
+            if Name in Sen[i]:
+                Occ[i] -= 2
+                index = i
+                for k in range(0,len(Sen)):
+                    if k != index:
+                        Occ[k] -= math.exp(-abs(index-k))
+
+    return Occ
+            
+#------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+
+def clean_up(a):
+    a = a.replace("- ","")
+    a = a.replace("\n","")
+    return a
+
+#------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+
+
+
+
+def Define_AbsOcc(NS,AO):
     Tense = []
     for i in range(0,len(NS)):
         Words = SA.sen_format(NS[i],SA.load_dico())
@@ -191,28 +225,24 @@ def Define_AbsOcc(NS,AO,DT,PF):
                 AO[i] -= 0.2
             if j.d[0].cgram == 'VER':
                 AO[i] -= 2
-    #        AbsOcc.append(Words.d[0].freqliv)
-    #[1].d[0].freqliv)    
     for i in range(0,len(AO)):
         if AO[i] == 'None':
             AO[i] = 0
-    T = []
-    T = [x for _,x in sorted(zip(AO,NS))]
-    Date = []
-    Date = [x for _,x in sorted(zip(AO,DT))]
-    Pays = []
-    Pays = [x for _,x in sorted(zip(AO,PF))]
-    AO = sorted(AO)    
-    return T,AO,Date,Pays
+
+    return AO
+
+#------------------------------------------------------------------------------
+    #-----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
 
 
 def find_evenement(Lines, Full, Name):
     DateTab = []
     PaysFinal = []
-    #DATE_JOURNAL = find_date(Full,DateTab)
-    [TailleSen,DATE,PaysFinal] = analyse_sen(Lines,DateTab,PaysFinal)
-    [Sorted_sen, Sorted_occ,DateTab,PaysFinal] = Occurence(Lines,DateTab,PaysFinal)
-    [Sorted_sen, Sorted_occ,DateTab,PaysFinal] = NameOccExp(Sorted_sen,Sorted_occ,Name,DateTab,PaysFinal)
+    [DATE,PaysFinal] = analyse_sen(Lines,DateTab,PaysFinal)
+    [Sorted_sen, Sorted_occ] = Occurence(Lines)
+    Sorted_occ = NameOccExp(Sorted_sen,Sorted_occ,Name)
     New_Sorted_sen = []
     for i in range(0,len(Sorted_sen)):
         New_Sorted_sen.append(clean_up(Sorted_sen[i]))
@@ -232,7 +262,7 @@ def find_evenement(Lines, Full, Name):
     
     
     
-    [NewSortedSen,NewSortedOcc,DateTab,PaysFinal] = Define_AbsOcc(NewSortedSen,NewSortedOcc,DateTab,PaysFinal)
+    NewSortedOcc = Define_AbsOcc(NewSortedSen,NewSortedOcc)
     
     
     TempTab = []
@@ -246,11 +276,7 @@ def find_evenement(Lines, Full, Name):
         TempTab = []
     return FinalTab
         
-import re
 
-with open("pays.txt", 'r') as pays_file:
-    Pays = pays_file.read()
-    Pays = Pays.split('\n')
 
 
 
